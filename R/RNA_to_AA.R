581ceca5aaf982f5f63a44cc98d6f93b489429c9
TilNelissen <s233043@dtu.dk>
2024-10-29 11:28:40 +0100

    Changed codons to codon_table

--- R/RNA_to_AA.R
+++ R/RNA_to_AA.R
@@ -9,6 +9,6 @@
 #' @examples input_vector <- c(ATGGCCUGCAAGUGGCGT)
 #' translation_RNA_2_AA(input_vector)
 translation_RNA_2_AA <- function(codons_input){
-  AA_seq <- paste0(codons[codons_input], collapse = "")
+  AA_seq <- paste0(codon_table[codons_input], collapse = "")
   return(AA_seq)
 }
